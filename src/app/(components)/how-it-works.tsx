a99a594ed18d2ac8f05525628ae93d27cab457b7
Naeem Abdul-Aziz <naeemabdulaziz202@gmail.com>
2025-08-25 13:18:06 +0000

    take inspiration from here. everything about this. but tailor it to our

--- src/app/(components)/how-it-works.tsx
+++ src/app/(components)/how-it-works.tsx
@@ -25,7 +25,7 @@ export function HowItWorks() {
                         <span>Design scalable tools to meet specific needs.</span>
                     </li>
                     <li className="flex items-start">
-                        <svg className="w-5 h-5 mr-2 mt-1 text-primary flex-shrink-0" fill="none" stroke="currentColor" viewBox="0_0_24_24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                        <svg className="w-5 h-5 mr-2 mt-1 text-primary flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                         <span>Our services work around the clock.</span>
                     </li>
                 </ul>
