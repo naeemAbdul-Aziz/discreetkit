4e220866718be524e04f65715d99eadb79887e78
Naeem Abdul-Aziz <naeemabdulaziz202@gmail.com>
2025-08-31 16:06:37 +0000

    is the bottom bar necessary on our landing page? now that we have a dedi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ import { TrustStats } from './(components)/trust-stats';
 import { CoverageMap } from './(components)/coverage-map';
 import { WhatsInTheKit } from './(components)/whats-in-the-kit';
 import { OurVision } from './(components)/our-vision';
-import { SummaryBar } from '@/components/summary-bar';
 import { HowItWorks } from './(components)/how-it-works';
 import { ProductBenefits } from './(components)/product-benefits';
 import { ProductSelector } from './(components)/product-selector';
@@ -29,7 +28,6 @@ export default function Home() {
       <CoverageMap />
       <Faq />
       <ClosingCta />
-      <SummaryBar />
     </div>
   );
 }
