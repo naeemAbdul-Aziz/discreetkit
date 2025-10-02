3f561fde764332d9278a46c03a637670945e6c4e
Naeem Abdul-Aziz <naeemabdulaziz202@gmail.com>
2025-10-02 16:27:17 +0000

    Try fixing this error: `Runtime Error: Error: AnimatePresence is not def

--- src/app/products/(components)/product-line-showcase.tsx
+++ src/app/products/(components)/product-line-showcase.tsx
@@ -10,7 +10,7 @@ import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext
 import { AddToCartManager } from '../../products/[id]/(components)/add-to-cart-manager';
 import type { Product } from '@/lib/data';
 import { cn } from '@/lib/utils';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
