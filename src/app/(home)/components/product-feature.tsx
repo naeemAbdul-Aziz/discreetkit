f30fe70af697583f8d79a1920d32d3f045f476aa
Naeem Abdul-Aziz <naeemabdulaziz202@gmail.com>
2025-10-02 12:30:58 +0000

    how can we make the features more converting? instead of this 'card' thi

--- src/app/(home)/components/product-feature.tsx
+++ src/app/(home)/components/product-feature.tsx
@@ -12,7 +12,6 @@ import { Button } from '@/components/ui/button';
 import { ArrowUpRight } from 'lucide-react';
 import { Badge } from '@/components/ui/badge';
 import { Card } from '@/components/ui/card';
-import { AddToCartManager } from '@/app/products/[id]/(components)/add-to-cart-manager';
 
 const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
