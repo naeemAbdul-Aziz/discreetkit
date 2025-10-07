c64243622081706983ac93a0cd6f37f7e0f70ac8
Naeem Abdul-Aziz <naeemabdulaziz202@gmail.com>
2025-10-07 14:39:31 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

--- src/app/admin/(components)/admin-shell.tsx
+++ src/app/admin/(components)/admin-shell.tsx
@@ -58,6 +58,8 @@ export function AdminShell({
   children: React.ReactNode;
   navItems: NavItem[];
 }) {
+  const [isSidebarOpen, setIsSidebarOpen] = React.useState(true);
+  
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       {/* Desktop Sidebar */}
@@ -111,7 +113,7 @@ export function AdminShell({
         </header>
 
         {/* Main Content */}
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-muted/20">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background">
           {children}
         </main>
       </div>
