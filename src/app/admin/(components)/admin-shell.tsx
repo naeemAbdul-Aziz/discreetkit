da4a86fafd2c06b1ee1d26cde906e7fdffb26300
Naeem Abdul-Aziz <naeemabdulaziz202@gmail.com>
2025-10-09 12:34:45 +0000

    ummm, when i navigate to another sidebar element it logs me out, fix thi

--- src/app/admin/(components)/admin-shell.tsx
+++ src/app/admin/(components)/admin-shell.tsx
@@ -115,12 +115,12 @@ export function AdminShell({
                     <Logo />
                     </Link>
                 </div>
-              <nav className="grid gap-2 text-lg font-medium">
+              <nav className="grid gap-2 text-lg font-medium flex-1 py-4">
                 {navItems.map(item => (
                   <NavLink key={item.href} item={item} isMobile />
                 ))}
               </nav>
-              <div className="mt-auto p-4">
+              <div className="mt-auto p-4 -mx-6 border-t">
                  <form action={logout}>
                     <Button variant="ghost" className="w-full justify-start text-lg">
                         <LogOut className="mr-2 h-5 w-5" />
