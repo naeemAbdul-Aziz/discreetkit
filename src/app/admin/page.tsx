ff1010bd3b8887962c63b463c9fcae430087c7e3
Naeem Abdul-Aziz <naeemabdulaziz202@gmail.com>
2025-10-13 06:32:33 +0000

    admin is still navigating us to admin/products

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,11 @@
 /**
  * @file page.tsx
  * @description The root page for the /admin route. It now immediately redirects
- *              the user to the dashboard, as authentication will be handled
+ *              the user to the login page, as authentication will be handled
  *              by the layout.
  */
 import { redirect } from 'next/navigation';
 
 export default function AdminRootPage() {
-    redirect('/admin/dashboard');
+    redirect('/admin/login');
 }
